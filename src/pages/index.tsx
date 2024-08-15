94d2fbfa97ad0a77b152ddeeef9a99c6b742aa8a
saudkhantc <saudkhantc@gmail.com>
2024-08-15 05:01:21 -0700

    code committed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import Image from 'next/image';
-import mainLogo from '@/public/images/main_logo.png';
-import { useRouter } from 'next/router';
-
+import Navbar from '@/components/navbar/Navbar';
 const index = () => {
-  const router= useRouter()
+  
   return (
-  <div className="absolute p-4  md:flex justify-between w-full">
-    <div>
-    <Image src={mainLogo} alt="Logo" width={150} height={40} />
-    </div>
-    
-    <div className='flex gap-4'>
-      <button className='border py-2 px-4 rounded-full'>Influencer</button>
-      <button className='border py-2 px-4 rounded-full'>Brand</button>
-    </div>
-    <div className='flex gap-4' ><button className='py-2 px-6 border rounded-full' onClick={()=>router.push('/signin')}>Log In</button>
-    <button className='py-2 px-6 border rounded-full bg-[#59FF93]' onClick={()=>router.push('/signup')}>Sign Up for free</button></div>
-  </div>
+  <>
+  <Navbar/>
+ 
+  </>
    )
 };
 
--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -25,7 +25,7 @@ const LoginPage = () => {
 
       <div className="w-full max-w-md p-8 bg-white rounded-lg relative">
         <div className="flex items-center space-x-4 mb-4 relative md:right-12">
-          <button className="text-lg relative bottom-3 hidden md:flex">
+          <button className="text-lg relative bottom-3 hidden md:flex" onClick={()=>router.push('/')}>
             <Image src={arrowBackDark} alt="Back" width={40} height={40} />
           </button>
           <div>
