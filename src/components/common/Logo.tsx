3bd57ae07e69ebab4e1648a2b1f08d607b1a0200
saudkhantc <saudkhantc@gmail.com>
2024-09-04 14:39:20 +0500

    issues resolved

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
 import mainLogo from '../../../public/images/main_logo.png';
+import { useRouter } from 'next/router';
 
-const Logo = () => (
-  <div className="absolute top-0 left-0 mt-4 ml-4">
-    <Image src={mainLogo} alt="Logo" width={150} height={40} />
+const Logo = () => {
+  const router = useRouter();
+  return (
+  <div className="absolute top-0 left-0 mt-4 ml-4 cursor-pointer">
+    <Image src={mainLogo} alt="Logo" width={150} height={40} onClick={()=> router.push('/')} />
   </div>
-);
+)};
 
 export default Logo;
