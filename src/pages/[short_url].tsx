b79ccd435602f4cdf0fce6b862ffdce356853698
saud khan <saudkhanbpk@gmail.com>
2024-09-30 23:12:32 -0700

    updated the reirection page

--- src/pages/[short_url].tsx
+++ src/pages/[short_url].tsx
@@ -1,4 +1,51 @@
+// import Image from "next/image";
+// import { useEffect } from "react";
+// import { useRouter } from "next/router";
+// import axios from "axios";
 
+// interface Props {}
+// const Redirect: React.FC<Props> = () => {
+//   const router = useRouter()
+
+//   const getReDirectLink = async (shortUrl: any) => {
+//     try {
+//       const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/${shortUrl}`);
+//       console.log({ link: response.data });
+//       if(response.data.redirectUrl){
+//         console.log("inside the if statement");
+        
+//           router.push(response.data.redirectUrl)
+//       }else{
+//         console.log("out of the if statement");
+        
+//       }
+//       // Handle the redirect or any other logic here
+//     } catch (error) {
+//       console.error('Error fetching redirect link:', error);
+//     }
+//   };
+
+
+//   useEffect(() => {
+//     const { short_url } = router.query;
+
+//     if (short_url) {
+//       // Call your function to handle redirection or fetch the link
+//       getReDirectLink(short_url);
+//     }
+//   }, [router.query]); // Add router.query as a dependency
+
+
+//   return (
+//     <div className="border h-screen flex items-center justify-center">
+//       <div className="flex justify-center items-center">
+//         <Image src="/images/Logo.svg" alt="Logo" height={100} width={300}/>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Redirect
 import Image from "next/image";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
@@ -6,32 +53,31 @@ import axios from "axios";
 
 interface Props {}
 
-
 // Detect if the user is on a mobile device
 const appLinkMappings = [
   {
     name: 'Instagram',
     urlPattern: /https:\/\/(www\.)?instagram\.com\/([^/?#&]+)/,
-    appScheme: 'instagram://',
+    appScheme: 'instagram://user?username=',
     webFallback: 'https://www.instagram.com/',
   },
   {
     name: 'YouTube',
     urlPattern: /https:\/\/(www\.)?youtube\.com\/watch\?v=([^&]+)/,
-    appScheme: 'youtube://watch?v=',
+    appScheme: 'youtube://',
     webFallback: 'https://www.youtube.com/watch?v=',
   },
   {
     name: 'Facebook',
     urlPattern: /https:\/\/(www\.)?facebook\.com\/([^/?#&]+)/,
-    appScheme: 'fb://faceweb/f?href=',
+    appScheme: 'fb://profile/',
     webFallback: 'https://www.facebook.com/',
   },
   {
     name: 'Amazon',
-    urlPattern: /https:\/\/(www\.)?amazon\.com\/(?:.+\/)?(dp|gp|product)\/([^/?#&]+)/,
-    appScheme: 'amazon://',
-    webFallback: 'https://www.amazon.com/',
+    urlPattern: /https:\/\/(www\.)?amazon\.com\/(?:.+\/)?dp\/([A-Z0-9]{10})/,
+    appScheme: 'amazon://detail?asin=',
+    webFallback: 'https://www.amazon.com/dp/',
   },
   {
     name: 'LinkedIn',
@@ -42,14 +88,8 @@ const appLinkMappings = [
   {
     name: 'Twitter',
     urlPattern: /https:\/\/(www\.)?twitter\.com\/([^/?#&]+)/,
-    appScheme: 'twitter://',
+    appScheme: 'twitter://user?screen_name=',
     webFallback: 'https://www.twitter.com/',
-  },
-  {
-    name: 'Telegram',
-    urlPattern: /https:\/\/(www\.)?t\.me\/([^/?#&]+)/,
-    appScheme: 'tg://',
-    webFallback: 'https://t.me/',
   },
 ];
 
