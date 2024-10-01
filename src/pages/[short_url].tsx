3ec739bc70d6235d0a880bce604dc9b62ecd5ea4
saud khan <saudkhanbpk@gmail.com>
2024-09-30 23:45:43 -0700

    updated the instagram urlPattern

--- src/pages/[short_url].tsx
+++ src/pages/[short_url].tsx
@@ -1,30 +1,168 @@
+// // import Image from "next/image";
+// // import { useEffect } from "react";
+// // import { useRouter } from "next/router";
+// // import axios from "axios";
+
+// // interface Props {}
+// // const Redirect: React.FC<Props> = () => {
+// //   const router = useRouter()
+
+// //   const getReDirectLink = async (shortUrl: any) => {
+// //     try {
+// //       const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/${shortUrl}`);
+// //       console.log({ link: response.data });
+// //       if(response.data.redirectUrl){
+// //         console.log("inside the if statement");
+        
+// //           router.push(response.data.redirectUrl)
+// //       }else{
+// //         console.log("out of the if statement");
+        
+// //       }
+// //       // Handle the redirect or any other logic here
+// //     } catch (error) {
+// //       console.error('Error fetching redirect link:', error);
+// //     }
+// //   };
+
+
+// //   useEffect(() => {
+// //     const { short_url } = router.query;
+
+// //     if (short_url) {
+// //       // Call your function to handle redirection or fetch the link
+// //       getReDirectLink(short_url);
+// //     }
+// //   }, [router.query]); // Add router.query as a dependency
+
+
+// //   return (
+// //     <div className="border h-screen flex items-center justify-center">
+// //       <div className="flex justify-center items-center">
+// //         <Image src="/images/Logo.svg" alt="Logo" height={100} width={300}/>
+// //       </div>
+// //     </div>
+// //   );
+// // };
+
+// // export default Redirect
 // import Image from "next/image";
 // import { useEffect } from "react";
 // import { useRouter } from "next/router";
 // import axios from "axios";
 
 // interface Props {}
+
+// // Detect if the user is on a mobile device
+// const appLinkMappings = [
+//   {
+//     name: 'Instagram',
+//     urlPattern: /https:\/\/(www\.)?instagram\.com\/(p\/[^/?#&]+|[^/?#&]+)/,
+//     appScheme: 'instagram://user?username=',
+//     webFallback: 'https://www.instagram.com/',
+//   },
+//   {
+//     name: 'YouTube',
+//     urlPattern: /https:\/\/(www\.)?youtube\.com\/watch\?v=([^&]+)/,
+//     appScheme: 'youtube://',
+//     webFallback: 'https://www.youtube.com/watch?v=',
+//   },
+//   {
+//     name: 'Facebook',
+//     urlPattern: /https:\/\/(www\.)?facebook\.com\/([^/?#&]+)/,
+//     appScheme: 'fb://profile/',
+//     webFallback: 'https://www.facebook.com/',
+//   },
+//   {
+//     name: 'Amazon',
+//     urlPattern: /https:\/\/(www\.)?amazon\.com\/(?:.+\/)?dp\/([A-Z0-9]{10})/,
+//     appScheme: 'amazon://detail?asin=',
+//     webFallback: 'https://www.amazon.com/dp/',
+//   },
+//   {
+//     name: 'LinkedIn',
+//     urlPattern: /https:\/\/(www\.)?linkedin\.com\/in\/([^/?#&]+)/,
+//     appScheme: 'linkedin://in/',
+//     webFallback: 'https://www.linkedin.com/in/',
+//   },
+//   {
+//     name: 'Twitter',
+//     urlPattern: /https:\/\/(www\.)?twitter\.com\/([^/?#&]+)/,
+//     appScheme: 'twitter://user?screen_name=',
+//     webFallback: 'https://www.twitter.com/',
+//   },
+// ];
+
+// function isMobile() {
+//   return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+// }
+
+// // Generate deep link for app and fallback to browser
+// function getAppLink(url: string) {
+//   for (let app of appLinkMappings) {
+//     const match = url.match(app.urlPattern);
+//     if (match) {
+//       const appDeepLink = app.appScheme + match[2];
+//       return {
+//         appDeepLink,
+//         fallbackLink: app.webFallback + match[2],
+//       };
+//     }
+//   }
+//   return { fallbackLink: url };
+// }
+
+// // Open the app link or fallback to browser
+// function openLink(url: string) {
+//   const { appDeepLink, fallbackLink } = getAppLink(url);
+  
+//   // Detect if the user has left the page (which indicates the app was opened)
+//   let appOpened = false;
+  
+//   function handleVisibilityChange() {
+//     if (document.hidden) {
+//       appOpened = true;  // The user left the page (likely opened the app)
+//     }
+//   }
+
+//   // Add visibility change listener
+//   document.addEventListener("visibilitychange", handleVisibilityChange);
+
+//   if (isMobile() && appDeepLink) {
+//     // Attempt to open the app
+//     window.location.href = appDeepLink;
+
+//     // Set a timeout to fallback to the browser version if the app isn't opened
+//     setTimeout(() => {
+//       if (!appOpened) {
+//         // If the user didn't leave the page, fallback to the browser
+//         window.location.href = fallbackLink;
+//       }
+//     }, 1500);  // Wait for 1.5 seconds before triggering the fallback
+//   } else {
+//     // If not on mobile or no app deep link, open the fallback link in the browser
+//     window.location.href = fallbackLink;
+//   }
+// }
+
 // const Redirect: React.FC<Props> = () => {
-//   const router = useRouter()
+//   const router = useRouter();
 
 //   const getReDirectLink = async (shortUrl: any) => {
 //     try {
 //       const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/${shortUrl}`);
 //       console.log({ link: response.data });
-//       if(response.data.redirectUrl){
+      
+//       if (response.data.redirectUrl) {
 //         console.log("inside the if statement");
-        
-//           router.push(response.data.redirectUrl)
-//       }else{
+//         openLink(response.data.redirectUrl);  // Handle app deep linking and fallback to browser here
+//       } else {
 //         console.log("out of the if statement");
-        
 //       }
-//       // Handle the redirect or any other logic here
 //     } catch (error) {
 //       console.error('Error fetching redirect link:', error);
 //     }
 //   };
-
 
 //   useEffect(() => {
 //     const { short_url } = router.query;
@@ -35,17 +173,17 @@
 //     }
 //   }, [router.query]); // Add router.query as a dependency
 
-
 //   return (
 //     <div className="border h-screen flex items-center justify-center">
 //       <div className="flex justify-center items-center">
-//         <Image src="/images/Logo.svg" alt="Logo" height={100} width={300}/>
+//         <Image src="/images/Logo.svg" alt="Logo" height={100} width={300} />
 //       </div>
 //     </div>
 //   );
 // };
 
-// export default Redirect
+// export default Redirect;
+
 import Image from "next/image";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
@@ -53,7 +191,7 @@ import axios from "axios";
 
 interface Props {}
 
-// Detect if the user is on a mobile device
+// Mapping URL patterns to app schemes and fallbacks
 const appLinkMappings = [
   {
     name: 'Instagram',
@@ -63,16 +201,16 @@ const appLinkMappings = [
   },
   {
     name: 'YouTube',
-    urlPattern: /https:\/\/(www\.)?youtube\.com\/watch\?v=([^&]+)/,
+    urlPattern: /https:\/\/(www\.)?youtube\.com\/(watch\?v=[^&]+|playlist\?list=[^&]+|channel\/[^/?#&]+)/,
     appScheme: 'youtube://',
-    webFallback: 'https://www.youtube.com/watch?v=',
-  },
+    webFallback: 'https://www.youtube.com/',
+  },  
   {
     name: 'Facebook',
-    urlPattern: /https:\/\/(www\.)?facebook\.com\/([^/?#&]+)/,
-    appScheme: 'fb://profile/',
+    urlPattern: /https:\/\/(www\.)?facebook\.com\/(posts|pages|[^/?#&]+)/,
+    appScheme: 'fb://',
     webFallback: 'https://www.facebook.com/',
-  },
+  },  
   {
     name: 'Amazon',
     urlPattern: /https:\/\/(www\.)?amazon\.com\/(?:.+\/)?dp\/([A-Z0-9]{10})/,
@@ -81,23 +219,24 @@ const appLinkMappings = [
   },
   {
     name: 'LinkedIn',
-    urlPattern: /https:\/\/(www\.)?linkedin\.com\/in\/([^/?#&]+)/,
-    appScheme: 'linkedin://in/',
-    webFallback: 'https://www.linkedin.com/in/',
-  },
+    urlPattern: /https:\/\/(www\.)?linkedin\.com\/(in\/[^/?#&]+|company\/[^/?#&]+|jobs\/view\/[^/?#&]+|posts\/[^/?#&]+)/,
+    appScheme: 'linkedin://',
+    webFallback: 'https://www.linkedin.com/',
+  },  
   {
     name: 'Twitter',
-    urlPattern: /https:\/\/(www\.)?twitter\.com\/([^/?#&]+)/,
-    appScheme: 'twitter://user?screen_name=',
+    urlPattern: /https:\/\/(www\.)?twitter\.com\/(i\/status\/[^/?#&]+|hashtag\/[^/?#&]+|[^/?#&]+)/,
+    appScheme: 'twitter://',
     webFallback: 'https://www.twitter.com/',
-  },
+  },  
 ];
 
+// Detect if the user is on a mobile device
 function isMobile() {
   return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 }
 
-// Generate deep link for app and fallback to browser
+// Generate deep link for the app and fallback to browser
 function getAppLink(url: string) {
   for (let app of appLinkMappings) {
     const match = url.match(app.urlPattern);
@@ -112,7 +251,7 @@ function getAppLink(url: string) {
   return { fallbackLink: url };
 }
 
-// Open the app link or fallback to browser
+// Open the app link or fallback to the browser
 function openLink(url: string) {
   const { appDeepLink, fallbackLink } = getAppLink(url);
   
@@ -120,7 +259,7 @@ function openLink(url: string) {
   let appOpened = false;
   
   function handleVisibilityChange() {
-    if (document.hidden) {
+    if (document.visibilityState === 'hidden') {
       appOpened = true;  // The user left the page (likely opened the app)
     }
   }
@@ -138,7 +277,7 @@ function openLink(url: string) {
         // If the user didn't leave the page, fallback to the browser
         window.location.href = fallbackLink;
       }
-    }, 1500);  // Wait for 1.5 seconds before triggering the fallback
+    }, 2000);  // Wait for 2 seconds before triggering the fallback
   } else {
     // If not on mobile or no app deep link, open the fallback link in the browser
     window.location.href = fallbackLink;
@@ -154,10 +293,10 @@ const Redirect: React.FC<Props> = () => {
       console.log({ link: response.data });
       
       if (response.data.redirectUrl) {
-        console.log("inside the if statement");
+        console.log("Redirecting to:", response.data.redirectUrl);
         openLink(response.data.redirectUrl);  // Handle app deep linking and fallback to browser here
       } else {
-        console.log("out of the if statement");
+        console.log("No redirect URL found");
       }
     } catch (error) {
       console.error('Error fetching redirect link:', error);
@@ -171,7 +310,7 @@ const Redirect: React.FC<Props> = () => {
       // Call your function to handle redirection or fetch the link
       getReDirectLink(short_url);
     }
-  }, [router.query]); // Add router.query as a dependency
+  }, [router.query]);
 
   return (
     <div className="border h-screen flex items-center justify-center">
